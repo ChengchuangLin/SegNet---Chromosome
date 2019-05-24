f91b430515bc8cb2ca634618a86889ddbb739d17
ChengchuangLin <linchengchuang@sworddata.cn>
2019-05-24 20:40:10 +0800

    update experiment

--- load_data.py
+++ load_data.py
@@ -9,34 +9,29 @@ def resize(arr):
     for i in range(arr.shape[0]):    
         img = sc.resize(arr[i], (88, 88), preserve_range=True)
         im.append(img)
-    
     im = np.array(im)
     return im
 
 
 def load_data():
     
-    data = h5py.File('chromosome.h5', 'r')   #loading data
+    data = h5py.File('LowRes_13434_overlapping_pairs.h5', 'r')   #loading data
     data = data.get('dataset_1')
     images = data[:, :, :, 0]
     labels = data[:, :, :, 1]
 
     images = resize(images)
     labels = resize(labels)
-    
-    
-    
+
+
     images = np.expand_dims(images, -1)
     labels = np.expand_dims(labels, -1)
-    
+
     print("Images shape:{}".format(images.shape))
     print("Labels shape:{}".format(labels.shape))
     
-    
-    
     images_train = images[:images.shape[0]-20]     #splitting into train and test sets
     images_test = images[images.shape[0]-20:]
-    
     labels_train = labels[:labels.shape[0]-20]
     labels_test = labels[labels.shape[0]-20:]
     
--- main.py
+++ main.py
@@ -5,32 +5,45 @@ import numpy as np
 from keras.callbacks import ModelCheckpoint, EarlyStopping
 from SegNet import *
 from load_data import *
+from keras.utils import multi_gpu_model
 
-X_train, y_train, X_test, y_test = load_data() 
-
+X_train, y_train, X_test, Y_test = load_data()
 
 num_classes = 4
 Y_train = to_categorical(y_train, num_classes)
 model = SegNet()
-model.compile(loss="categorical_crossentropy", optimizer='adam', metrics=['accuracy'])
-model.summary()
-
-
-
-model_checkpoint = ModelCheckpoint('Weights.h5', monitor='val_loss', save_best_only=True)
-early_stopping = EarlyStopping(patience=2, verbose=2)
-
-
+prallel_model = multi_gpu_model(model, gpus=2)
+prallel_model.compile(
+    loss="categorical_crossentropy",
+    optimizer='adam',
+    metrics=['accuracy'])
+prallel_model.summary()
+# model_checkpoint = ModelCheckpoint('Weights.h5',
+#                                    monitor='val_loss',
+#                                    save_best_only=True)
+early_stopping = EarlyStopping(patience=10,
+                               mode='auto',
+                               monitor='val_loss',
+                               restore_best_weights=True,
+                               verbose=1)
 print('Fitting model...')
-history = model.fit(X_train, Y_train, batch_size=16, epochs=10, validation_split=0.05, 
-                    shuffle=True, callbacks=[model_checkpoint, early_stopping])
-
-
-model.load_weights('Weights.h5')
-Y_test = model.predict(X_test, verbose=1)
+history = prallel_model.fit(X_train, Y_train,
+                            batch_size=50,
+                            epochs=10,
+                            validation_split=0.2,
+                            shuffle=True,
+                            callbacks=[early_stopping],
+                            # validation_data=(X_test, Y_test),
+                            verbose=1)
+train_score = prallel_model.evaluate(X_train, Y_train, verbose=1)
+print('train_score', train_score)
+Y_test = to_categorical(Y_test, num_classes)
+verify_score = prallel_model.evaluate(X_test, Y_test, verbose=1)
+print('verify_score', verify_score)
+# prallel_model.load_weights('Weights.h5')
+Y_test = prallel_model.predict(X_test, verbose=1)
 Y_test = Y_test.reshape(20, 88, 88, 4)
 Y = np.argmax(Y_test, axis=-1)
-
 
 print('Saving predicted masks to files...')
 
